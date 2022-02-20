b674fd6bcb33925dcae8fde1216ecb3d81c516e7
Thrivikram Mudunuri <23713701+tmudunuri@users.noreply.github.com>
2022-02-20 11:30:14 -0500

    Update server

--- server.ts
+++ server.ts
@@ -1,23 +1,22 @@
 import express, {Request, Response} from 'express';
 import UserController from './controllers/UserController';
 import TuitController from "./controllers/TuitController";
-import UserDao from './daos/UserDao';
 import TuitDao from './daos/TuitDao';
 import mongoose from "mongoose";
 import bodyParser from "body-parser";
 
-const app = express();
-
 mongoose.connect('mongodb+srv://admin:' + process.env.DB_PASSWORD + '@cluster0.aazb2.mongodb.net/tuiter?retryWrites=true&w=majority')
+const app = express();
 app.use(bodyParser.json())
 
 app.get('/hello', (req: Request, res: Response) =>
     res.send('Hello World!'));
 
-const userDao = new UserDao();
 const tuitDao = new TuitDao();
-const userController = new UserController(app, userDao);
+const userController = UserController.getInstance(app);
 const tuitController = new TuitController(app, tuitDao);
+
+// create RESTful Web service API
 
 const PORT = 4000;
 app.listen(process.env.PORT || PORT);
