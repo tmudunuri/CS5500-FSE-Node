6a32c81760d50e42034803a13f1aeefc804fe736
Thrivikram Mudunuri <23713701+tmudunuri@users.noreply.github.com>
2022-03-18 17:04:32 -0400

    Update DB host

--- server.ts
+++ server.ts
@@ -22,7 +22,7 @@ var cors = require('cors')
 const PROTOCOL = "mongodb+srv";
 const DB_USERNAME = process.env.DB_USERNAME;
 const DB_PASSWORD = process.env.DB_PASSWORD;
-const HOST = "cluster0.ayiv2.mongodb.net";
+const HOST = "cluster0.aazb2.mongodb.net";
 const DB_NAME = "tuiter";
 const DB_QUERY = "retryWrites=true&w=majority";
 const connectionString = `${PROTOCOL}://${DB_USERNAME}:${DB_PASSWORD}@${HOST}/${DB_NAME}?${DB_QUERY}`;
