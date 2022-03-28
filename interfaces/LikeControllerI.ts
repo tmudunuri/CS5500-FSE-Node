53b0203ea06bfac61e8576819308683fdfeea1a2
Thrivikram Mudunuri <23713701+tmudunuri@users.noreply.github.com>
2022-03-28 14:43:07 -0400

    Declare and implement controller method for my dislike screen
    Declare a method to fetch tuits disliked by a user
    and implement the same to be used by an API.

--- interfaces/LikeControllerI.ts
+++ interfaces/LikeControllerI.ts
@@ -9,5 +9,7 @@ export default interface LikeControllerI {
 
     findAllUsersThatDislikedTuit(req: Request, res: Response): void;
 
+    findAllTuitsDislikedByUser(req: Request, res: Response): void;
+
     userTogglesTuitDislikes(req: Request, res: Response): void;
 };
